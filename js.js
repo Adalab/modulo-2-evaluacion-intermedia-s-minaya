a1f2a042add779046fb9cc017bb6b39867f57d0e
Eilith404 <minaya.sofia@gmail.com>
2025-10-30 10:06:11 +0100

    Add event to playBtn and show results

--- js.js
+++ js.js
@@ -1,1 +1,40 @@
 "use strict";
+
+// SECCIÓN QUERY SELECTOR //
+const selectEl = document.querySelector(".js_selectEl");
+const playBtn = document.querySelector(".js_playBtn");
+const messageEl = document.querySelector(".js_messageEl");
+const playerScoreEl = document.querySelector(".js_playerScoreEl");
+const computerScoreEl = document.querySelector(".js_computerScoreEl");
+const roundInfoEl = document.querySelector(".js_roundInfoEl");
+
+// SECCIÓN FUNCIONES //
+function getRandomNumber(max) {
+  return 1 + parseInt(Math.random() * max);
+}
+
+// SECCIÓN EVENTOS //
+playBtn.addEventListener("click", (ev) => {
+  const playerMove = selectEl.value;
+  console.log("Jugador elige:", playerMove);
+
+  // Numero aleatorio computer
+
+  const randomNumber = getRandomNumber(9);
+
+  console.log("Número aleatorio:", randomNumber);
+
+  let computerMove = "";
+
+  if (randomNumber <= 3) {
+    computerMove = "Piedra";
+  } else if (randomNumber >= 7) {
+    computerMove = "Papel";
+  } else {
+    computerMove = "Tijera";
+  }
+
+  console.log("Computer choice:", computerMove);
+});
+
+// SECCIÓN ACCIONES //
